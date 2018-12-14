50f4cd36c22fc7cc6dc60991684f452042d2f1ed
meooow25 <soumiksarkar.3120@gmail.com>
2018-12-14 19:47:57 +0530

    Add Mo's algorithm

--- Codeforces/86D.cpp
+++ Codeforces/86D.cpp
@@ -1,4 +1,3 @@
-
 // Powerful array
 // http://codeforces.com/contest/86/problem/D
 // Mo's algorithm
@@ -8,21 +7,16 @@
 using namespace std;
 
 const int MAX_N = 2e5 + 5;
-const int MAX_V = 1e6 + 5;
+const int MAX_VAL = 1e6 + 5;
 
-int N, Q, a[MAX_N];
+int N, Q, A[MAX_N];
 long long ans[MAX_N];
 struct query {
     int l, r, id;
 } q[MAX_N];
 
-int bsize, cnt[MAX_V];
+int cnt[MAX_VAL];
 long long cur;
-
-bool compare(query &q1, query &q2) {
-    int b1 = q1.l / bsize, b2 = q2.l / bsize;
-    return b1 != b2 ? b1 < b2 : b1 & 1 ? q1.r < q2.r : q2.r < q1.r;
-}
 
 inline void add(int x) {
     cur += x * (cnt[x]++ << 1 | 1LL);
@@ -33,20 +27,20 @@ inline void rem(int x) {
 }
 
 void mo() {
-    bsize = 2*sqrt(N) + 0.5;
-    sort(q, q+Q, compare);
+    int bsize = sqrt(N) + 0.5;
+    auto comp = [bsize](query &q1, query &q2) {
+        int b1 = q1.l / bsize, b2 = q2.l / bsize;
+        return b1 != b2 ? b1 < b2 : b1 & 1 ? q1.r < q2.r : q2.r < q1.r;
+    };
+    sort(q, q + Q, comp);
     
     int l = 1, r = 0;
     cur = 0;
-    for(int i=0; i<Q; i++) {
-        while(l < q[i].l)
-            rem(a[l++]);
-        while(l > q[i].l)
-            add(a[--l]);
-        while(r < q[i].r)
-            add(a[++r]);
-        while(r > q[i].r)
-            rem(a[r--]);
+    for(int i = 0; i < Q; i++) {
+        while(l < q[i].l) rem(A[l++]);
+        while(l > q[i].l) add(A[--l]);
+        while(r < q[i].r) add(A[++r]);
+        while(r > q[i].r) rem(A[r--]);
         ans[q[i].id] = cur;
     }
 }
@@ -55,17 +49,17 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     
     cin >> N >> Q;
-    for(int i=1; i<=N; i++)
-        cin >> a[i];
+    for(int i = 1 ; i <= N; i++)
+        cin >> A[i];
         
-    for(int i=0; i<Q; i++) {
+    for(int i = 0; i < Q; i++) {
         q[i].id = i;
         cin >> q[i].l >> q[i].r;
     }
     
     mo();
     
-    for(int i=0; i<Q; i++)
+    for(int i = 0; i < Q; i++)
         cout << ans[i] << "\n";
     
     return 0;

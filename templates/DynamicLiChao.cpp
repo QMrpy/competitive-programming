37605d5319ccb2f8a6955fcce38855c0187051aa
meooow25 <soumiksarkar.3120@gmail.com>
2018-12-21 04:17:39 +0530

    Update Dynamic Li Chao

--- templates/DynamicLiChao.cpp
+++ templates/DynamicLiChao.cpp
@@ -4,33 +4,29 @@
 
 // https://e-maxx-eng.appspot.com/geometry/convex_hull_trick.html
 
-const long long INF = 1e18;
-
-struct line {
-    long long m, c;
-
-    inline long long eval(long long x) const {
-        return m * x + c;
-    }
-};
-
 namespace LiChao {
+    const long long INF = 1e18;
+    const int MAX_NODES = 1e6;
+    struct line {
+        long long m, c;
+        inline long long eval(long long x) { return m * x + c; }
+    };
     struct node {
         int s, e, m;
-        line cur;
-        int lt, rt;
-    } pool[MAX_NODES];
+        line cur{0, INF};
+        int lt = 0, rt = 0;
+        node() {}
+        node(int ss, int ee): s(ss), e(ee), m((ss + ee) >> 1) {}
+    } P[MAX_NODES];
     int p = 1;
 
     int newnode(int ss, int ee) {
-        node &n = pool[p];
-        n.s = ss; n.e = ee; n.m = (ss + ee) >> 1;
-        n.cur = {0, INF};
+        P[p] = node(ss, ee);
         return p++;
     }
 
     long long query(int i, int x) {
-        node &n = pool[i];
+        node &n = P[i];
         long long y = n.cur.eval(x);
         if (n.s == n.e) return y;
         if (x < n.m && n.lt) y = min(y, query(n.lt, x));
@@ -38,12 +34,9 @@ namespace LiChao {
         return y;
     }
 
-    void addline(int i, line l) {
-        node &n = pool[i];
-        if (n.cur.c == INF) {
-            n.cur = l;
-            return;
-        }
+    void insert(int i, line l) {
+        node &n = P[i];
+        if (n.cur.c == INF) { n.cur = l; return; }
         bool b1 = l.eval(n.s) < n.cur.eval(n.s);
         bool b2 = l.eval(n.m) < n.cur.eval(n.m);
         bool b3 = l.eval(n.e) < n.cur.eval(n.e);
@@ -52,10 +45,10 @@ namespace LiChao {
         if (n.s == n.e) return;
         if (b1 != b2) {
             if (!n.lt) n.lt = newnode(n.s, n.m);
-            addline(n.lt, l);
+            insert(n.lt, l);
         } else {
             if (!n.rt) n.rt = newnode(n.m + 1, n.e);
-            addline(n.rt, l);
+            insert(n.rt, l);
         }
     }
 }
